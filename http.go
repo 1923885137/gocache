546bbeae1887a1d188249b104522e014632d5a94
中南大学智能刘嘉俊 <1923885137@qq.com>
2022-09-09 19:15:33 +0800

    add picker

--- http.go
+++ http.go
@@ -1,19 +1,29 @@
 package gocache
 
 import (
+	"GoCache/consistenthash"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_gocache/"
+const (
+	defaultBasePath = "/_gocache/"
+	defaultReplicas = 50
+)
 
 // HTTPPool implements PeerPicker for a pool HTTP peers.
 type HTTPPool struct {
 	//"https://example.net:8000"
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex //guard peers and httpGetters
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter
 }
 
 // NewHTTPPool initializes an HTTP pool of peers
@@ -24,8 +34,31 @@ func NewHTTPPool(self string) *HTTPPool {
 	}
 }
 
+// Set updates the pool's list of peers
+func (p *HTTPPool) set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+// PickPeer picks a peer according to key
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
 // Log info with server name
-func (p *HTTPPool) log(format string, v ...interface{}) {
+func (p *HTTPPool) Log(format string, v ...interface{}) {
 	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
 }
 
@@ -34,7 +67,7 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
-	p.log("%s %s", r.Method, r.URL.Path)
+	p.Log("%s %s", r.Method, r.URL.Path)
 
 	//  /$basePath/$groupname/$key
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
@@ -60,3 +93,32 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+type httpGetter struct {
+	baseURL string
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Server returned:%v", res.Status)
+	}
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
